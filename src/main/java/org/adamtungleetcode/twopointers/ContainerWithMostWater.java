4cf329067bb145afd6352b60d80966587175f3aa
sltung98 <tungsiuleung@gmail.com>
2024-03-07 10:50:40 +0800

    improve solution to container with most water

--- src/main/java/org/adamtungleetcode/twopointers/ContainerWithMostWater.java
+++ src/main/java/org/adamtungleetcode/twopointers/ContainerWithMostWater.java
@@ -4,16 +4,27 @@ public class ContainerWithMostWater {
     public static void main(String[] args) {
         System.out.println(new ContainerWithMostWater().maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
         System.out.println(new ContainerWithMostWater().maxArea(new int[] {2,3,10,5,7,8,9}));
+        System.out.println(new ContainerWithMostWater().maxArea(new int[] {2,3,4,5,18,17,6}));
     }
     public int maxArea(int[] height) {
-        int maxArea = 0;
-        for (int i = 0; i < height.length; i++) {
-            int j = i + 1;
-            int heightI = height[i];
-            int heightJ = height[j];
-            int areaIJ = Math.min(heightI, heightJ) * (j - i);
-            if (areaIJ > maxArea) {
-                maxArea = areaIJ;
+        int left = 0;
+        int right = height.length - 1;
+        int heightAtLeft = height[left];
+        int heightAtRight = height[right];
+        int maxArea = (right - left) * Integer.min(heightAtLeft, heightAtRight);
+        while (left < right) {
+            //System.out.printf("left: %d, right: %d, heightAtLeft: %d, heightAtRight: %d\n", left, right, heightAtLeft, heightAtRight);
+            int newArea;
+            if (heightAtLeft < heightAtRight) {
+                left++;
+                heightAtLeft = height[left];
+            } else {
+                right--;
+                heightAtRight = height[right];
+            }
+            newArea = (right - left) * Integer.min(heightAtLeft, heightAtRight);
+            if (newArea >= maxArea) {
+                maxArea = newArea;
             }
         }
         return maxArea;
